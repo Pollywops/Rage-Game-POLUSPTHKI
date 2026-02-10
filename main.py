ec067cf62c6e601ad5bd4153ee6b584fac5f485f
Pollywops <2122233@hageveld.nl>
2026-02-10 18:30:00 +0100

    klik op R voor super bullets

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import pygame,sys,math
 
+import pygame,sys,math
+import json
 
 pygame.font.init()
 pygame.mixer.init()
 
 shotgun_shot = pygame.mixer.Sound('sounds/Shotgun_shot.mp3')
 shotgun_empty = pygame.mixer.Sound('sounds/Empty.mp3')
-
 pygame.mixer.set_num_channels(40)
 SCREENSIZE = [800,800]
 FPS = 60
@@ -27,11 +27,12 @@ GRAY = (128,128,128)
 
 screen = pygame.display.set_mode(SCREENSIZE,flags=pygame.RESIZABLE, vsync=1)
 clock = pygame.time.Clock()
+bullet_type = 'NORMAL'
 
 player = pygame.sprite.GroupSingle()
 gun = pygame.sprite.GroupSingle()
 blocks = pygame.sprite.Group()
-
+buttons = pygame.sprite.Group()
 
 def lines_to_matrix(lines):
     matrix = []
@@ -55,6 +56,21 @@ def load_from_json(path="level.json"):
 
 level = load_from_json()
 
+class Camera:
+    def __init__(self, screen_size):
+        self.resize(screen_size)
+        self.offset = pygame.Vector2(0, 0)
+
+    def resize(self, screen_size):
+        self.screen_w, self.screen_h = screen_size
+
+    def update(self, target_rect):
+        self.offset.x = target_rect.centerx - self.screen_w / 2
+        self.offset.y = target_rect.centery - self.screen_h / 2
+
+    def apply_rect(self, rect):
+        # world -> screen
+        return rect.move(-self.offset.x, -self.offset.y)
 
 
 class Player(pygame.sprite.Sprite):
@@ -114,11 +130,10 @@ class Player(pygame.sprite.Sprite):
                     self.vely = 0
                     self.touchingceiling = True
                     self.friction = 30
-                    #print(self.friction)
             gun.sprite.bullets = 2
 
         if self.velx > 0:
-            if self.velx <= 0.03:
+            if self.velx <= 0.005:
                 self.velx = 0
 
             else:
@@ -126,7 +141,7 @@ class Player(pygame.sprite.Sprite):
                 if self.velx <= 0.03:
                     self.velx = 0
         if self.velx < 0:
-            if self.velx >= 0.03:
+            if self.velx >= 0.005:
                 self.velx = 0
 
             else:
@@ -135,29 +150,6 @@ class Player(pygame.sprite.Sprite):
                     self.velx = 0
   
     def update(self):
-        keys = pygame.key.get_pressed()
-        
-        #handeling deacaleration
-
-        # if keys[pygame.K_w]:
-        #     self.vely = -5
-                
-            
-        # elif keys[pygame.K_s]:
-        #     self.vely = 5
-        
-      
-                
-        if keys[pygame.K_d]:
-            self.velx = 5
-        
-        elif keys[pygame.K_a]:
-            self.velx = -5
-
-
-
-
-
         self.physics()
 
         screen.blit(self.image,[self.rect.left - self.rect.centerx + SCREENSIZE[0]/2,self.rect.top - self.rect.centery + SCREENSIZE[1]/2])
@@ -173,17 +165,23 @@ class Gun(pygame.sprite.Sprite):
         self.rect.center = (10,10)
         self.flipped = False
         self.bullets = 2
+        self.bullet_type = 'NORMAL'
+        self.bullet_strength = 1
     
     def shoot(self):
-        self.bullets = self.bullets - 1
-        #print(self.bullets)
-        if self.bullets >= 0:
+
+        if self.bullet_type == 'NORMAL':
+            self.bullet_strength = 1
+        elif self.bullet_type == 'SUPER':
+            self.bullet_strength = 1.5
+        if self.bullets > 0:
+            self.bullets = self.bullets - 1
             shotgun_shot.play()
             if player.sprite.vely > 0:
                 player.sprite.vely = 0
                 print(player.sprite.vely)
-            addvelx = -math.cos(self.angle)*10
-            addvely = -math.sin(self.angle)*10
+            addvelx = -math.cos(self.angle)*10*self.bullet_strength
+            addvely = -math.sin(self.angle)*10*self.bullet_strength
             player.sprite.add_vel(addvelx,addvely)
         else:
             shotgun_empty.play()
@@ -224,11 +222,38 @@ class Blocks(pygame.sprite.Sprite):
     def update(self):
         screen.blit(self.image, [self.rect.left - player.sprite.rect.centerx + SCREENSIZE[0] / 2,
                                  self.rect.top - player.sprite.rect.centery + SCREENSIZE[1] / 2])
-        
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self,x,y,w,h,Transparent, color,fontsize, fontoffsetX, fontoffsetY, text):
+        super().__init__()
+        self.image = pygame.Surface((w,h))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x , y]
+        self.font = pygame.font.SysFont('Arial', fontsize)
+        self.fontoffsetX = fontoffsetX
+        self.fontoffsetY = fontoffsetY
+        self.Transparent = Transparent
+        self.text = text
+    def update(self):
+        if not self.Transparent:
+            screen.blit(self.image, [self.rect.topleft,self.rect.topleft])
+        text_surface = self.font.render(self.text, True, BLACK)
+        screen.blit(text_surface, [self.rect.topleft[0] + self.fontoffsetX, self.rect.topleft[1]])
+
+cam = Camera(SCREENSIZE)
 player.add(Player(500,0,50,50,BLUE))
 gun.add(Gun(10,10))
 blocks.add(Blocks(400,700,1000,100,GREEN))
 blocks.add(Blocks(100,100,200,500,GREEN))
+
+button1 = Button(500, 50, 175, 30, True, GREEN,
+                 30, 5,fontoffsetY= -3,text= 'BULLETS:  ' + str(gun.sprite.bullets))
+button2 = Button(500,100, 175, 30, True, GREEN,
+                 30, 5,fontoffsetY= -3,text= 'BULLET TYPE: ' + str(gun.sprite.bullet_type))
+
+buttons.add(button1)
+buttons.add(button2)
 
 
 # for y,i in enumerate(level):
@@ -247,19 +272,30 @@ while True:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
                 gun.sprite.shoot()
-
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                if gun.sprite.bullet_type == 'SUPER':
+                    gun.sprite.bullet_type = 'NORMAL'
+                else:
+                    gun.sprite.bullet_type = 'SUPER'
         if event.type == pygame.VIDEORESIZE:
             SCREENSIZE = [event.w, event.h]
 
             screen = pygame.display.set_mode(SCREENSIZE, flags=pygame.RESIZABLE, vsync=1)
 
+
+    cam.update(player.sprite.rect)
     screen.fill(AQUA)
     blocks.update()
     player.update()
+    button1.text = 'BULLETS: ' + str(gun.sprite.bullets)
+    button2.text = 'BULLET TYPE: ' + str(gun.sprite.bullet_type)
+    buttons.update()
     px, py = player.sprite.rect.center
     cx, cy = px - SCREENSIZE[0], py - SCREENSIZE[1]
     #print(px, py, cx, cy)
     gun.update(px,py)
+
         
     
     
